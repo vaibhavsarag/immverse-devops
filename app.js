79b50b7f60b1e199297bd47243f345c5dc0eb964
EC2 Default User <ec2-user@ip-172-31-40-40.ap-south-1.compute.internal>
2026-06-05 10:16:05 +0000

    final test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD testing successful');
+  res.send('CI/CD testing successfully done.');
 });
 
 app.listen(3000, () => {
