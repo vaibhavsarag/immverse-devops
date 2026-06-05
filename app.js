5729af00ffb0e930eeb48fff3173b9b3da274317
EC2 Default User <ec2-user@ip-172-31-40-40.ap-south-1.compute.internal>
2026-06-05 08:02:35 +0000

    version 3 added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD version 2 Pipeline Running Successfully');
+  res.send('CI/CD version 3 Pipeline Running Successfully');
 });
 
 app.listen(3000, () => {
