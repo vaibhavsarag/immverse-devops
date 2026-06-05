bc993fc0c65903efae1a754be45aa407df16dde7
EC2 Default User <ec2-user@ip-172-31-40-40.ap-south-1.compute.internal>
2026-06-05 08:45:48 +0000

    Testing app.js changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD version 5 Pipeline Running Successfully, where is my offer letter, XD');
+  res.send('CI/CD final version');
 });
 
 app.listen(3000, () => {
