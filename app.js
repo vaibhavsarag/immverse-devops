2c4f3d0e6567eb9ffa222e080f38dcd235da9833
EC2 Default User <ec2-user@ip-172-31-40-40.ap-south-1.compute.internal>
2026-06-05 10:27:09 +0000

    testing webhooks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD testing successfully done.');
+  res.send('CI/CD testing successfully done now testing webhooks.');
 });
 
 app.listen(3000, () => {
